c1486d363b12816969485db3d8f0e8d5f5966f61
jkokeny4 <jkok2707@gmail.com>
2026-09-24 21:45:19 -0600

    Target installed Android 15 SDK

--- individual assignments/Assignment2/app/build.gradle.kts
+++ individual assignments/Assignment2/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.assignment2"
-    compileSdk = 37
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.assignment2"
         minSdk = 24
-        targetSdk = 37
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
